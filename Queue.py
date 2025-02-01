528d6527060ceb68a88235ba97f5ad1613738e33
Vinay Kumar Devnani <88175142+Vkdevnani@users.noreply.github.com>
2025-02-01 20:10:53 +0530

    Create Queue.py

---
+++ Queue.py
@@ -0,0 +1,29 @@
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop() if self.stack2 else None
+
+    def peek(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1] if self.stack2 else None
+
+    def empty(self) -> bool:
+        return not self.stack1 and not self.stack2
+
+q = MyQueue()
+q.push(1)
+q.push(2)
+print(q.peek())  
+print(q.pop())   
+print(q.empty()) 
