64403d146a538cc4e40d9f855d694806217483eb
Vinay Kumar Devnani <88175142+Vkdevnani@users.noreply.github.com>
2025-02-01 19:56:56 +0530

    Jump_Game.py

---
+++ jump.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def jump(nums: List[int]) -> int:
+    jumps = 0
+    current_end = 0
+    farthest = 0
+    
+    for i in range(len(nums) - 1):
+        farthest = max(farthest, i + nums[i])
+        
+        if i == current_end:
+            jumps += 1
+            current_end = farthest
+            
+            if current_end >= len(nums) - 1:
+                break
+    
+    return jumps
+
+nums = [2, 3, 1, 1, 4]
+print(jump(nums))
